1faf28eed960957a2661759962a95f8dcd597b5d
Meng Xu <86040828+mengxu98@users.noreply.github.com>
2024-02-21 23:04:15 +0800

    Use `fast.prcomp` instead `prcomp`

--- R/setMethod_get.embedding.R
+++ R/setMethod_get.embedding.R
@@ -9,13 +9,15 @@ get.embedding.default <- function(
     object,
     cores = 1,
     ...) {
-  pca_res <- stats::prcomp(object)
+  pca_res <- gmodels::fast.prcomp(object)
   pca_res <- pca_res$x
   rownames(pca_res) <- rownames(object)
   umap_res <- suppressMessages(uwot::umap(object, n_threads = cores))
   rownames(umap_res) <- rownames(object)
-  embedding <- list(pca_res, umap_res)
-  names(embedding) <- c("PCA", "UMAP")
+  embedding <- list(
+    "PCA" = pca_res,
+    "UMAP" = umap_res
+  )
 
   return(embedding)
 }
@@ -30,8 +32,10 @@ get.embedding.Seurat <- function(object, ...) {
   rownames(pca_res) <- colnames(object)
   umap_res <- object@reductions$umap@cell.embeddings
   rownames(umap_res) <- colnames(object)
-  embedding <- list(pca_res, umap_res)
-  names(embedding) <- c("PCA", "UMAP")
+  embedding <- list(
+    "PCA" = pca_res,
+    "UMAP" = umap_res
+  )
 
   return(embedding)
 }
