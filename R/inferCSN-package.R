4fcc6be63a11a9599c3328003d55dde31928146c
Meng Xu <86040828+mengxu98@users.noreply.github.com>
2024-02-24 22:28:22 +0800

    Update package information

--- R/inferCSN-package.R
+++ R/inferCSN-package.R
@@ -1,7 +1,4 @@
-#' inferCSN: Inferring Cell-Specific Gene Regulatory Network
-#'
-#' @description
-#'  A method for inferring cell-specific gene regulatory network from single-cell sequencing data.
+#' @title Inferring cell-type-specific gene regulatory networks from single-cell omics data
 #'
 #' @author Meng xu (Maintainer), \email{mengxu98@qq.com}
 #'
