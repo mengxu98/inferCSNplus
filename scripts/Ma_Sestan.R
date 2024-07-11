60af2be6168d262a5f038fe6c663371eeaf36607
Meng xu <86040828+mengxu98@users.noreply.github.com>
2024-07-11 17:11:47 +0800

    delete

--- scripts/Ma_Sestan.R
+++
@@ -1,8 +0,0 @@
-source("scripts/functions.R")
-library(Seurat)
-
-Ma_Sestan <- readRDS("~/HAR/brain_data/Ma_Sestan_mat.rds")
-seurat_object <- seurat_function(
-  Ma_Sestan,
-  re_create_object = TRUE
-)
